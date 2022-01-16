d8ba0cdb1c9af201149598ababf84c99546c0737
Chris Werkheiser <cjwerk@gmail.com>
2022-01-15 17:02:36 -0800

    working good

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
+
+const WithSpinner =
+  (WrappedComponent) =>
+  ({ isloading, ...otherProps }) => {
+    return isloading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
+
+export default WithSpinner;
